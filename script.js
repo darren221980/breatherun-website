d6f9ae7442af7a33f88ee3eb88dc18000b865365
Darren Bromwich <dazbromwich@gmail.com>
2026-07-28 12:19:05 +0100

    website updates

---
+++ script.js
@@ -0,0 +1,23 @@
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add("active");
+
+        }
+
+    });
+
+}, {
+
+    threshold:0.15
+
+});
+
+document.querySelectorAll(".reveal").forEach(section => {
+
+    observer.observe(section);
+
+});
